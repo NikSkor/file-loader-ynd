e699cf31a98d692437daefdf3d547c255b49fc26
NikSkor <mr-zerx@yandex.ru>
2023-08-03 11:16:09 +0500

    clean token

--- src/components/API/const.ts
+++ src/components/API/const.ts
@@ -1,2 +1,2 @@
-export const token = 'y0_AgAAAAAAuZgcAADLWwAAAADpTQDR5aRGEktlRF6krySp7iY4p1Q4DSM';
+export const token = 'вставьте свой OAuth токен для работы приложения';
 export const URL_API = 'https://cloud-api.yandex.net/v1/disk/resources/upload';
